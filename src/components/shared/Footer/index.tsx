a6e1c527816ec5f419412539f6de3a02b3c2f3be
Hieronimus Ferry <ferryhieronimus@gmail.com>
2023-07-03 01:04:16 +0700

    fix: add opacity in footer

--- src/components/shared/Footer/index.tsx
+++ src/components/shared/Footer/index.tsx
@@ -2,7 +2,7 @@ import GlowContainer from "../GlowContainer";
 
 export default function Footer() {
   return (
-    <GlowContainer>
+    <GlowContainer opacity={0.25}>
       <div className='flex flex-col min-w-full items-center justify-center bg-[#1c2025] p-12 sm:p-18 lg:p-24 gap-4'>
         <div className='text-white text-3xl sm:text-6xl '>
           Let's collaborate
