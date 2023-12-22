4adc560d86b417cb94d54d4dbcd04e593240b736
Hieronimus Ferry <ferryhieronimus@gmail.com>
2023-12-22 23:12:16 +0700

    chore: update cv

--- src/app/components/organisms/about-section/index.tsx
+++ src/app/components/organisms/about-section/index.tsx
@@ -40,7 +40,7 @@ export default function AboutSection() {
           </Typography>
 
           <a
-            href='https://drive.google.com/file/d/1rCh8wXwEYRUZTUljHVNiuV2Y2xRJi98T/view?usp=sharing'
+            href='https://drive.google.com/file/d/1tcDraRH5KOt8ibaVb6uH24AhTrMOWgzr/view?usp=sharing'
             target='_blank'
             rel='noopener noreferrer'
           >
